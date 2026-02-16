f91a53697fe424be54b908eb6f3b22d272cf1b29
Christian Montoya <chrimonlo@gmail.com>
2026-02-16 15:54:53 -0500

    feat(templates): Add the js file

---
+++ templates/my-element.js
@@ -0,0 +1,39 @@
+class myElement extends HTMLElement {
+    //Primer ciclo de vida, constructor
+    constructor() {
+        super();
+    }
+
+    getTemplate(){
+        const template = document.createElement('template');
+        template.innerHTML = `
+            <section>
+                <h2>Uso de template</h2>
+                <div>
+                    <p>Ejemplo de como se crea y usa una template en web components<p>
+                </div
+            </section>
+            ${this.getStyles()}
+        `;
+        return template;
+    }
+    getStyles(){
+        return `
+        <style>
+            h2 {
+                color: red;
+            }
+        </style>
+        `
+    }
+    //En este metodo clonamos todos los elementos anidados del template
+    render(){
+        const template = this.getTemplate();
+        this.appendChild(template.content.cloneNode(true));
+    }
+    //Segundo ciclo de vida
+    connectedCallback() {
+        this.render();
+    }
+}
+customElements.define('my-element', myElement);
